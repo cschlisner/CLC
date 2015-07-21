7a1ff915d837f0fb02aeb91767529dc6cb24589d
Cole Schlisner <cschlisner@gmail.com>
2015-07-20 21:01:58 -0600

    Removed old code

--- Arduino/CLC/CLC.ino
+++ Arduino/CLC/CLC.ino
@@ -5,8 +5,6 @@ void setup() {
   analogWrite(9,255);
 }
 
-float chainLength = 0.0, chainCount = 0.0, totalChainLength = 0.0;
-float chainAvg = 0.0;
 int level = 0;
 void loop() {
   while(Serial.available()){
