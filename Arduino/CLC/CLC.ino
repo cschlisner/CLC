fb583fd3195289856b45e258ee29e6e0dd92f719
Cole Schlisner <cschlisner@gmail.com>
2015-07-21 23:54:19 -0600

    Simplification

--- Arduino/CLC/CLC.ino
+++ Arduino/CLC/CLC.ino
@@ -1,16 +1,11 @@
-
 void setup() {
-  // initialize serial:
+  // initialize serial and turn lights ON:
   Serial.begin(9600);
   analogWrite(9,255);
 }
 
-int level = 0;
 void loop() {
   while(Serial.available()){
-    level = Serial.read();
-    analogWrite(9,level);
+    analogWrite(9, Serial.read());
   }
 }
-
-
